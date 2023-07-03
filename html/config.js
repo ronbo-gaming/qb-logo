e593e83c3df9ad8bed0e5fb864cc321a776a9ed1
Ronbo-Gaming <67126123+ronbo-gaming@users.noreply.github.com>
2023-07-03 18:08:33 +0200

    Update config.js

--- html/config.js
+++ html/config.js
@@ -1,4 +1,4 @@
-var ImgLINK = "https://media.discordapp.net/attachments/1120066266728763442/1125149910610755624/logo.png?width=399&height=467";
+var ImgLINK = "https://media.discordapp.net/attachments/1120066266728763442/1125149910610755624/logo.png?width=577&height=676";
 
 
 
