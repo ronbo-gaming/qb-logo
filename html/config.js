f6194d863563e89b5c38926ad78394e489d01579
Ronbo-Gaming <67126123+dawoud-rgb@users.noreply.github.com>
2023-07-03 15:08:51 +0200

    Add files via upload

---
+++ html/config.js
@@ -0,0 +1,16 @@
+var ImgLINK = "https://media.discordapp.net/attachments/1120066266728763442/1120806844034728026/brotherspng.png?width=676&height=676";
+
+
+
+
+
+
+
+
+
+
+
+
+
+//dont touch
+document.write('<img src=' + ImgLINK + ' width="150" height="150">')
